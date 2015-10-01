c53c4216f76fc55959072343ab2caa5e2084105e
gardners <paul@servalproject.org>
2015-10-02 04:14:42 +0930

    fix !0 escape sequence

--- Firmware/radio/serial.c
+++ Firmware/radio/serial.c
@@ -148,6 +148,7 @@ serial_interrupt(void) __interrupt(INTERRUPT_UART0)
 				param_set(PARAM_TXPOWER,1);
 			} else if ((c=='0') && last_was_bang ) {
 				// clear TX buffer
+				last_was_bang=0;
 				while (BUF_NOT_EMPTY(rx)) {
 					BUF_REMOVE(rx,c);
 				}
