a30d27755b764e1bfdb827c6fc5ad969f452b0c4
gardners <paul@servalproject.org>
2015-12-29 15:23:19 +1030

    ... on second thoughts, leave !F alone, and add !R to reset radio parameters to default.

--- Firmware/radio/serial.c
+++ Firmware/radio/serial.c
@@ -164,9 +164,6 @@ serial_interrupt(void) __interrupt(INTERRUPT_UART0)
 				else if (c<'y') pins_user_set_value(c-'s',1);
 #endif				
 			} else if ((c=='F') && last_was_bang ) {
-				// Reset radio to default settings (like AT&F)
-				param_default();
-			} else if ((c=='I') && last_was_bang ) {
 				// Identify radio firmware by series of checksums of flash
 				last_was_bang=0;
 				flash_report_summary();
@@ -181,6 +178,9 @@ serial_interrupt(void) __interrupt(INTERRUPT_UART0)
 			} else if ((c=='L') && last_was_bang ) {
 				last_was_bang=0;
 				param_set(PARAM_TXPOWER,1);
+			} else if ((c=='R') && last_was_bang ) {
+				// Reset radio to default settings (like AT&F)
+				param_default();
 			} else if ((c=='V') && last_was_bang ) {
 				// Provide version info, to allow quick detection of CSMA
 				// firmware
