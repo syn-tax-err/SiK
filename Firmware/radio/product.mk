95b05f8174743381a4f842b6e8e1fbd401ffef8e
gardners <paul@servalproject.org>
2016-12-16 17:24:12 +0100

    don't fail on stupid compiler warning

--- Firmware/radio/product.mk
+++ Firmware/radio/product.mk
@@ -35,7 +35,7 @@ PRODUCT		 =	radio~$(BOARD)
 PRODUCT_DIR	:=	$(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 CFLAGS		+=	-DAPP_VERSION_HIGH=$(VERSION_MAJOR) -DAPP_VERSION_LOW=$(VERSION_MINOR)
-CFLAGS		+=	$(CFLAG_MODEL) --opt-code-speed --Werror --std-sdcc99 --fomit-frame-pointer
+CFLAGS		+=	$(CFLAG_MODEL) --opt-code-speed --std-sdcc99 --fomit-frame-pointer
 #CFLAGS		+=	--fverbose-asm 
 PRODUCT_SUPPORT_BANKING = 1
 INC_DIR_AES	 = AES
