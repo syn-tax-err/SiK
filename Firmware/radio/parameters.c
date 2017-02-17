cd779a64b0defeac9393fca8263c625fd8416d52
gardners <paul@servalproject.org>
2017-02-17 16:54:43 +1030

    set I2C delay default in parameters.c to match in i2c.c

--- Firmware/radio/parameters.c
+++ Firmware/radio/parameters.c
@@ -67,7 +67,7 @@ __code const struct parameter_info {
 	{"LBT_RSSI",        80},
 	{"MANCHESTER",      0},
 	{"RTSCTS",          0},
-	{"I2CDELAY",    50000}
+	{"I2CDELAY",       50}
 };
 
 /// In-RAM parameter store.
